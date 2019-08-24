1c282eb81362da03eee95d50157bf39bf83ed177
Marc Tönsing <marc@marc.tv>
2019-08-24 19:46:26 +0200

    no verbose

--- sync-ramdisk.sh
+++ sync-ramdisk.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# run me as a scheduled task every minute as the same use the files belong to 
-rsync -avhW --no-compress --delete --exclude '@eaDir' /volume1/public/ramdisk/ /volume1/public/ramdisk-local/
+# run me as a scheduled task every minute as the same use the files belong to
+rsync -ahW --no-compress --delete /volume1/public/ramdisk/ /volume1/public/ramdisk-local/
