f81042dc303837a7a5cae9714d03a925cd0fc6c5
Marc Tönsing <marc@marc.tv>
2019-08-24 18:20:10 +0200

    added context

--- mount-ramdisk.sh
+++ mount-ramdisk.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# run me as a triggered task on boot up as root 
 mount -t tmpfs -o size=25% ramdisk /volume1/public/ramdisk
 sleep 1
-rsync -avhW --no-compress /volume1/public/ramdisk-local/ /volume1/public/ramdisk/ 
+rsync -avhW --no-compress /volume1/public/ramdisk-local/ /volume1/public/ramdisk/
--- sync-ramdisk.sh
+++ sync-ramdisk.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+# run me as a scheduled task every minute as the same use the files belong to 
 rsync -avhW --no-compress --delete --exclude '@eaDir' /volume1/public/ramdisk/ /volume1/public/ramdisk-local/
