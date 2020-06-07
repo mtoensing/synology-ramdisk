c740675262a9ed25de5524190668220fbdf573f8
Marc Tönsing <mtoensing@users.noreply.github.com>
2020-06-07 08:38:36 +0200

    Update sync-ramdisk.sh

--- sync-ramdisk.sh
+++ sync-ramdisk.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# run me as a scheduled task every minute as the same use the files belong to
+# run me as a scheduled task every minute with the same user the files belong to.
 rsync -ahW --no-compress --delete /volume1/public/ramdisk/ /volume1/public/ramdisk-local/
